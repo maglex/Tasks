95643d2413ffc84fbdccbf13bf3c67075a369944
Aleksey Averkov <aleksey.averkov@gmail.com>
2012-04-13 18:04:05 +0700

    print HW

---
+++ TasksClient/src/AppClientMain.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Admin
+ * Date: 13.04.12
+ * Time: 17:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class AppClientMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
